7361ba63527d44a8aeb9bfaaae8b9d549edf1ba8
Rslan <russen333@gmail.com>
2025-07-10 09:48:51 +0200

    feat: change changelog( User Service)

--- user-service/src/main/java/com/healthcare/user_service/config/AuditConfig.java
+++ user-service/src/main/java/com/healthcare/user_service/config/AuditConfig.java
@@ -1,18 +1,18 @@
-package com.healthcare.user_service.config;
-
-
-import com.healthcare.user_service.audit.AuditorAwareImpl;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
-public class AuditConfig {
-
-    @Bean
-    public AuditorAware<String> auditorProvider() {
-        return new AuditorAwareImpl();
-    }
-}
+//package com.healthcare.user_service.config;
+//
+//
+//import com.healthcare.user_service.audit.AuditorAwareImpl;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.domain.AuditorAware;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//
+//@Configuration
+//@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+//public class AuditConfig {
+//
+//    @Bean
+//    public AuditorAware<String> auditorProvider() {
+//        return new AuditorAwareImpl();
+//    }
+//}
