a1701f9380a0d3761f6750853d5311dd2603284a
Rslan <russen333@gmail.com>
2026-02-17 18:30:23 +0100

    test: add singer to api gateway3

---
+++ api-gateway/src/main/java/com/healthcare/api_gateway/filter/signing/interfaces/UserContextSigner.java
@@ -0,0 +1,13 @@
+package com.healthcare.api_gateway.filter.signing.interfaces;
+
+
+import java.time.Duration;
+import java.util.List;
+
+public interface UserContextSigner {
+    String sign(String userId,
+                List<String> roles,
+                String requestId,
+                Duration ttl);
+}
+
